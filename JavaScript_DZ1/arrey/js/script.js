612648c6a6d960ef5443585fecf704e1973e37be
Olena Naumets <elennau7@gmail.com>
2016-03-10 21:30:19 +0200

    JavaScript_DZ_1

---
+++ JavaScript_DZ1/arrey/js/script.js
@@ -0,0 +1,26 @@
+﻿var arrNameUser = [];
+
+  for (var i = 0; i < 5; i++) {
+  var j = i + 1;
+   arrNameUser[i] = prompt('Введите ' + j + '-е имя:' , ' ');
+    }
+
+ var NameUser = prompt('Введите имя пользователя:', '');
+
+ var searchUser = 0;
+
+  for (var i = 0; i < arrNameUser.length; i++) {
+  if (arrNameUser[i] == NameUser) {
+  searchUser++;
+} 
+}
+
+if (searchUser > 0) {
+  alert (NameUser + ', Вы успешно вошли!');
+} else {
+  alert ('Ошибка!!!');
+  }
+
+console.log ('arr = ', arrNameUser);
+
+console.log ('NameUser= ', NameUser);
---
+++ JavaScript_DZ1/pow/js/script.js
@@ -0,0 +1,22 @@
+﻿var x = prompt('Введите число:', 0);
+var n = prompt('Введите степень числа:', 0);
+
+
+function pow(x, n) {
+ 
+  var result = 1;
+
+  for (var i = 0; i < n; i++) {
+   result = result*x;
+	
+  }
+
+  return result;
+}
+
+var rez = pow (x, n);
+
+alert (x + ' в степени ' + n + ' равно ' + rez);
+
+console.log ('rez = ', rez);
+
